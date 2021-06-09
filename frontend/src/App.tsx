007144004b7e4a62c9901c955a4e8ee0f9222c77
Flcable <flcable@gmail.com>
2021-06-09 17:18:06 +0100

    Handling routes not found

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import { HomePage } from './HomePage';
 import { AskPage } from './AskPage';
 import { SearchPage } from './SearchPage';
 import { SignInPage } from './SignInPage';
+import { NotFoundPage } from './NotFoundPage';
 
 import { fontFamily, fontSize, gray2 } from './Styles';
 import React from 'react';
@@ -26,6 +27,7 @@ function App() {
           <Route path="search" element={<SearchPage/>} />
           <Route path="ask" element={<AskPage/>} />
           <Route path="signin" element={<SignInPage/>} />
+          <Route path="*" element={<NotFoundPage/>} />
         </Routes>
         <HomePage />
       </div>
