47e24c0af964234986186b9e11519f861387a48a
Flcable <flcable@gmail.com>
2021-06-09 15:37:23 +0100

    Route's Implement

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,37 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
 
 import { fontFamily, fontSize, gray2 } from './Styles';
+import React from 'react';
 
 function App() {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Routes>
+          <Route path="" element={<HomePage/>} />
+          <Route path="search" element={<SearchPage/>} />
+          <Route path="ask" element={<AskPage/>} />
+          <Route path="signin" element={<SignInPage/>} />
+        </Routes>
+        <HomePage />
+      </div>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
